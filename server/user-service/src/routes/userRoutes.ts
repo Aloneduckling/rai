22582823e29c03c7ad72b92192127671424c0608
Aloneduckling <shantanukaushik12@gmail.com>
2024-10-22 19:24:50 +0530

    init oauth with google logic
    currently not working, have to find an alternative way....kitna sar dard
    hai bhai oauth

--- server/user-service/src/routes/userRoutes.ts
+++ server/user-service/src/routes/userRoutes.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import authUser from '../middlewares/authUser';
-import { signup, signin, verifyEmail, sendOTP } from '../controllers/userControllers';
+import { signup, signin, verifyEmail, sendOTP, signinWithGoogle } from '../controllers/userControllers';
+import passport from 'passport';
+import '../utils/passport'; //passport config
+
 
 const userRouter = express.Router();
+
+userRouter.use(passport.initialize());
 
 /*
     TODO
@@ -21,6 +26,14 @@ userRouter.post('/signup', signup);
 userRouter.post('/verify', authUser, verifyEmail);
 
 userRouter.get('/otp', authUser, sendOTP);
+
+userRouter.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+userRouter.get('/auth/google/callback', passport.authenticate('google', {
+    failureRedirect: '/signin'
+}), signinWithGoogle);
 
 userRouter.post('/signin', signin);
 
