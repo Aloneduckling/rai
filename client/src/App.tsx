89db710bd1126ede5df29552cd94d1c0b271b0ff
Aloneduckling <shantanukaushik12@gmail.com>
2024-10-30 21:42:08 +0530

    oauth testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="text-3xl text-blue-500 text-center">hello world</div>
+  return (
+    <div className="py-4">
+      <h1 className="text-center">Signin with google</h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Signin With google</button>
+    </div>
+  )
 }
 
 export default App
